4ba982f4888ad5b2441c42e2896f9698d3fd3330
Peter Faretra <pete@entropyandinsight.com>
2019-12-04 14:29:02 +0000

    finished script
    done!

---
+++ script.js
@@ -0,0 +1,9 @@
+function ShowPopup() {
+	var popup = document.getElementById('popup');
+	popup.classList.add('show');
+}
+
+function HidePopup() {
+	var popup = document.getElementById('popup');
+	popup.classList.remove('show');
+}
